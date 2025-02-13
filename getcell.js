673539c72c19f3a2b989bca6409f9451caf3f72c
0xDgtl <104255050+0xDgtl@users.noreply.github.com>
2025-02-13 14:44:50 +0700

    auto select between TH and EN

--- getcell.js
+++ getcell.js
@@ -1,11 +1,12 @@
 // Replace with your Web App URL
-    const webAppUrl = 'https://script.google.com/macros/s/AKfycbxUIwP-USuJHKjCKcNSQiiOqNHeY6t6Df4LUto4CHTH0FTJTeENycXSQ2X3IFQ2za7few/exec';
+    const webAppUrlTH = 'https://script.google.com/macros/s/AKfycbxUIwP-USuJHKjCKcNSQiiOqNHeY6t6Df4LUto4CHTH0FTJTeENycXSQ2X3IFQ2za7few/exec';
+    const webAppUrlEN = 'https://script.google.com/macros/s/AKfycby32woQpH13ze5D35uY7NbhRW66hdrFHlAyIN3sg1eNrPu7d6qtmQvD8NGenDbaXr50/exec';
 
     // Global object to store cell data
     let cellData = {};
 
-    function loadData(cells) {
-      fetch(`${webAppUrl}?cells=${cells.join(',')}`)
+    function loadData(cells, webAppUrl) {
+      fetch(`webAppUrl?cells=${cells.join(',')}`)
         .then(response => response.json())
         .then(data => {
           cellData = data;
@@ -36,7 +37,15 @@
     // Fetch the data once when the page loads
     window.onload = () => {
       const cellsToFetch = Array.from(document.querySelectorAll('[data-cell]')).map(el => el.getAttribute('data-cell'));
+      const pathSegments = window.location.pathname.split('/').filter(Boolean);
+      const firstPath = pathSegments.length > 0 ? `/${pathSegments[0]}/` : '/';
+         console.log(firstPath);
+
       if (cellsToFetch.length > 0) {
-        loadData(cellsToFetch);
+          if (firstPath == '/en/'){
+             loadData(cellsToFetch, webAppUrlEN);
+          }else{
+             loadData(cellsToFetch, webAppUrlTH);
+          }
       }
     };
